d90a5c50cd6afee89ac655800612136b3e93f5b8
DanielSahar <daniel.sahar@cycognito.com>
2026-04-14 20:27:42 +0300

    Add cookies usage limit feature

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
 
 const app = express()
 app.use(express.static('public'))
+app.use(cookieParser())
 
 app.get('/api/bug', (req, res) =>
 
@@ -29,15 +32,25 @@ app.get('/api/bug/save', (req, res) => {
 })
 
 app.get('/api/bug/:_id', (req, res) => {
+
     const bug_id = req.params._id
-    
+    const {visitedBugs = []} = req.cookies
+
+
+    if(visitedBugs.length === 3 && !visitedBugs.includes(bug_id)) {
+        return res.status(401).send('Wait for a bit')
+    }
+
+   if(!visitedBugs.includes(bug_id)) visitedBugs.push(bug_id)
+        console.log(visitedBugs);
+
+    res.cookie('visitedBugs', visitedBugs, {maxAge: 1000 * 20})
     bugService.get(bug_id)
         .then(bug => {res.send(bug)})
         .catch(err => {
             loggerService.error(err)
             res.status(404).send('Cant find bug')
         })
-
 })
 
 
